1d5512dabeb36e69fa3a0c242484f1d0e6cc5356
arialghifari <arialghifari99@gmail.com>
2022-07-21 12:57:39 +0800

    Fix netlify favicon bug

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store";
+import Favicon from "react-favicon";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Favicon url="/favicon.ico" />
     <BrowserRouter>
       <Provider store={store}>
         <App />
